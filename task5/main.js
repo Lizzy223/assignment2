d94eda446988e3c4153b431d7566b8cfa9d8c264
Lizzy223 <54336149+Lizzy223@users.noreply.github.com>
2020-06-29 08:58:25 +0100

    task5

---
+++ task5/main.js
@@ -0,0 +1,19 @@
+// $(function(){
+// 	$('.datepicker').datepicker({
+// 		autoclose: true;
+// 		todayHighlight: true;
+// 	}).datepicker('update', new Date());
+// });
+
+// $(document).ready(function(){
+// 	$('#mydate').datepicker({
+// 		todayHighlight:true,
+// 		orientation: "bottom left",
+// 		format:"mm/dd/yy"
+// 		container:"bdc"
+// 	})
+// })
+
+
+
+//document.getElementById("mydate").type = "date";1
